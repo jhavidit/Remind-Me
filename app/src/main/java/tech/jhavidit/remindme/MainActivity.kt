5c189567c606fdb4c0f48c6d5eeac29f935f2c37
jhavidit <jhavidit@gmail.com>
2021-04-22 20:29:35 +0530

    Added - Bottom Navigation

--- app/src/main/java/tech/jhavidit/remindme/MainActivity.kt
+++ app/src/main/java/tech/jhavidit/remindme/MainActivity.kt
@@ -2,10 +2,41 @@ package tech.jhavidit.remindme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import tech.jhavidit.remindme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        bottomNavigationView = binding.bottomNav
+        navController = findNavController(R.id.NavHostFragment)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.icon_notes -> {
+                    navController.navigate(R.id.notesFragment)
+                    true
+                }
+                R.id.icon_events -> {
+                    navController.navigate(R.id.eventsFragment)
+                    true
+                }
+                R.id.icon_settings -> {
+                    navController.navigate(R.id.settingsFragment)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+
+        }
     }
 }
