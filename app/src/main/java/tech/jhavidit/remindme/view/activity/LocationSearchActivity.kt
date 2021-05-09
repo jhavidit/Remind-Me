b8484d3561ad34b0469f24f8f1b16572dea4a204
jhavidit <jhavidit@gmail.com>
2021-05-09 13:00:24 +0530

    Added -  Location Permission Required

--- app/src/main/java/tech/jhavidit/remindme/view/activity/LocationSearchActivity.kt
+++ app/src/main/java/tech/jhavidit/remindme/view/activity/LocationSearchActivity.kt
@@ -1,18 +1,22 @@
 package tech.jhavidit.remindme.view.activity
 
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import tech.jhavidit.remindme.R
@@ -20,6 +24,20 @@ import tech.jhavidit.remindme.databinding.ActivityLocationSearchBinding
 
 
 class LocationSearchActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var map: GoogleMap? = null
+    private var cameraPosition: CameraPosition? = null
+    private lateinit var placesClient: PlacesClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+
+    private var lastKnownLocation: Location? = null
+    private var likelyPlaceNames: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAddresses: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAttributions: Array<List<*>?> = arrayOfNulls(0)
+    private var likelyPlaceLatLngs: Array<LatLng?> = arrayOfNulls(0)
+
     private val lat = 26.4841
     private val lon = 80.2759
     private lateinit var binding: ActivityLocationSearchBinding
@@ -28,6 +46,12 @@ class LocationSearchActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_location_search)
         val apiKey = getString(R.string.api_key)
+
+//        if (savedInstanceState != null) {
+//            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION)
+//            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION)
+//        }
+
         if (!Places.isInitialized()) {
             Places.initialize(applicationContext, apiKey);
         }
--- app/src/main/java/tech/jhavidit/remindme/view/fragments/LocationReminderFragment.kt
+++ app/src/main/java/tech/jhavidit/remindme/view/fragments/LocationReminderFragment.kt
@@ -1,15 +1,19 @@
 package tech.jhavidit.remindme.view.fragments
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import tech.jhavidit.remindme.R
 import tech.jhavidit.remindme.databinding.FragmentLocationReminderBinding
 import tech.jhavidit.remindme.view.activity.LocationSearchActivity
 import tech.jhavidit.remindme.viewModel.NotesViewModel
@@ -17,8 +21,10 @@ import tech.jhavidit.remindme.viewModel.NotesViewModel
 
 class LocationReminderFragment : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentLocationReminderBinding
-    private lateinit var viewModel : NotesViewModel
+    private lateinit var viewModel: NotesViewModel
     private val args: LocationReminderFragmentArgs by navArgs()
+    private val LOCATION_REQUEST_CODE = 1
+    private val PERMISSION_REQUEST_CODE = 200
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,10 +34,67 @@ class LocationReminderFragment : BottomSheetDialogFragment() {
         binding = FragmentLocationReminderBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(NotesViewModel::class.java)
         binding.locationPicker.setOnClickListener {
-            val intent = Intent(requireContext(),LocationSearchActivity::class.java)
-            startActivity(intent)
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), PERMISSION_REQUEST_CODE
+                )
+            } else {
+                val intent = Intent(requireContext(), LocationSearchActivity::class.java)
+                startActivity(intent)
+            }
         }
         return binding.root
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode)
+        {
+            200 ->
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    val intent = Intent(requireContext(),LocationSearchActivity::class.java)
+                    startActivity(intent)
+                }
+            else
+                {
+                    val alertDialog  : AlertDialog.Builder =  AlertDialog.Builder(requireContext())
+                    alertDialog.setMessage("You need to provide location permission to access this feature. Kindly enable it from settings")
+                    alertDialog.setCancelable(true)
+                    alertDialog.setPositiveButton(
+                        "Ok", DialogInterface.OnClickListener { dialog, which ->
+                            val intent =
+                                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            val uri: Uri =
+                                Uri.fromParts("package", activity?.packageName, null)
+                            intent.data = uri
+                            startActivity(intent)
+                        }
+                    )
+                    alertDialog.setNegativeButton(
+                        "Cancel", DialogInterface.OnClickListener { dialog, which ->
+                            dialog.cancel()
+                        }
+                    )
+                    val alert = alertDialog.create()
+                    alert.show()
+
+                    }
+
+        }
+    }
 }
