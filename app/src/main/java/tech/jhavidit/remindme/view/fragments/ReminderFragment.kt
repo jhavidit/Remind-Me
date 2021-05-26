eacd6828be790b283930735c15825f7ac39f8e24
jhavidit <jhavidit@gmail.com>
2021-05-26 14:39:22 +0530

    Added - Reminder List Screen

--- app/src/main/java/tech/jhavidit/remindme/view/fragments/ReminderFragment.kt
+++ app/src/main/java/tech/jhavidit/remindme/view/fragments/ReminderFragment.kt
@@ -5,17 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import tech.jhavidit.remindme.R
 import tech.jhavidit.remindme.databinding.FragmentRemindersBinding
+import tech.jhavidit.remindme.model.NotesModel
+import tech.jhavidit.remindme.view.adapters.LocationReminderListAdapter
+import tech.jhavidit.remindme.view.adapters.NotesListAdapter
+import tech.jhavidit.remindme.view.adapters.TimeReminderListAdapter
+import tech.jhavidit.remindme.viewModel.NotesViewModel
 
 
 class ReminderFragment : Fragment() {
-    private lateinit var binding : FragmentRemindersBinding
+    private lateinit var binding: FragmentRemindersBinding
+    private lateinit var locationAdapter: LocationReminderListAdapter
+    private lateinit var timeAdapter: TimeReminderListAdapter
+    private lateinit var viewModel: NotesViewModel
+    private lateinit var timeReminderList: List<NotesModel>
+    private lateinit var locationReminderList: List<NotesModel>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding =  FragmentRemindersBinding.inflate(inflater, container, false)
+        locationAdapter = LocationReminderListAdapter()
+        timeAdapter = TimeReminderListAdapter()
+        binding = FragmentRemindersBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(NotesViewModel::class.java)
+
+        viewModel.readAllData.observe(viewLifecycleOwner, Observer { notes ->
+            timeReminderList = notes?.filter {
+                it.reminderTime != null
+            } ?: listOf()
+
+            locationReminderList = notes?.filter {
+                it.locationName != null
+            } ?: listOf()
+
+            binding.recyclerView.adapter = timeAdapter
+            timeAdapter.setNotes(timeReminderList)
+
+            binding.rbReminder.setOnCheckedChangeListener { group, checkedId ->
+                if (checkedId == R.id.rb_location) {
+                    binding.recyclerView.adapter = locationAdapter
+                    locationAdapter.setNotes(locationReminderList)
+                } else if (checkedId == R.id.rb_time) {
+                    binding.recyclerView.adapter = timeAdapter
+                    timeAdapter.setNotes(timeReminderList)
+                }
+            }
+        })
+
+
         return binding.root
     }
 
