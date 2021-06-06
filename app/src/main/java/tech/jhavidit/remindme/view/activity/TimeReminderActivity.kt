09221d1531c012b3d4f28c7b908742ceaf5c2061
jhavidit <jhavidit@gmail.com>
2021-06-06 18:58:44 +0530

    Added - Reminder Activity Layout

--- app/src/main/java/tech/jhavidit/remindme/view/activity/TimeReminderActivity.kt
+++ app/src/main/java/tech/jhavidit/remindme/view/activity/TimeReminderActivity.kt
@@ -3,27 +3,33 @@ package tech.jhavidit.remindme.view.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import tech.jhavidit.remindme.R
 import tech.jhavidit.remindme.databinding.ActivityTimeReminderBinding
 import tech.jhavidit.remindme.service.AlarmService
+import tech.jhavidit.remindme.viewModel.NotesViewModel
 
 class TimeReminderActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityTimeReminderBinding
+    private lateinit var binding: ActivityTimeReminderBinding
+    private lateinit var viewModel : NotesViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_time_reminder)
-        binding.dismiss.setOnClickListener{
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_time_reminder)
+        viewModel = ViewModelProvider(this).get(NotesViewModel::class.java)
+        binding.cancelReminder.setOnClickListener{
             val intentService = Intent(applicationContext, AlarmService::class.java)
             applicationContext.stopService(intentService)
             finish()
         }
-        if(intent.extras?.getString("dismiss") == "dismiss")
-        {
-            val intentService = Intent(applicationContext, AlarmService::class.java)
-            applicationContext.stopService(intentService)
+        binding.openApp.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
             finish()
         }
+
     }
 }
