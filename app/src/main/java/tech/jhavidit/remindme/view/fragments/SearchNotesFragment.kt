16b767df6b12b8f16c20a77f5e7f3362b4733f47
jhavidit <jhavidit@gmail.com>
2021-06-17 01:37:47 +0530

    Added - search note functionality added

--- app/src/main/java/tech/jhavidit/remindme/view/fragments/SearchNotesFragment.kt
+++ app/src/main/java/tech/jhavidit/remindme/view/fragments/SearchNotesFragment.kt
@@ -1,23 +1,30 @@
 package tech.jhavidit.remindme.view.fragments
 
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.SearchView
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import tech.jhavidit.remindme.R
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import tech.jhavidit.remindme.databinding.FragmentSearchNotesBinding
 import tech.jhavidit.remindme.util.log
+import tech.jhavidit.remindme.view.adapters.SearchNoteAdapter
 import tech.jhavidit.remindme.viewModel.NotesViewModel
 
 
 class SearchNotesFragment : Fragment() {
     private lateinit var binding: FragmentSearchNotesBinding
     private lateinit var viewModel: NotesViewModel
+    private lateinit var adapter: SearchNoteAdapter
 
 
     override fun onCreateView(
@@ -27,24 +34,48 @@ class SearchNotesFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSearchNotesBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(NotesViewModel::class.java)
-        binding.searchNote.setOnQueryTextListener(object :
-            androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                viewModel.searchNote("%$query%").observe(viewLifecycleOwner, Observer {
-                    log("searching $query $it")
-                })
-                return true
-            }
+        adapter = SearchNoteAdapter()
+        binding.backBtn.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.searchNote.requestFocus()
+        val inputMethodManager =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+        binding.clearBtn.setOnClickListener {
+            binding.searchNote.setText("")
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                viewModel.searchNote("%$newText%").observe(viewLifecycleOwner, Observer {
-                    log("search $newText ${it.toString()}")
-                })
-                return true
-            }
+        binding.searchNote.addTextChangedListener(textWatcher)
 
-        })
         return binding.root
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            s?.let { str ->
+                if (str.isEmpty()) {
+                    binding.clearBtn.visibility = GONE
+                    adapter.setNotes(listOf())
+                } else {
+                    binding.clearBtn.visibility = VISIBLE
+                    viewModel.searchNote("%$str%").observe(viewLifecycleOwner, Observer {
+                        log("searching $str $it")
+                        adapter.setNotes(it)
+
+                    })
+                }
+
+            }
+        }
     }
 
 
